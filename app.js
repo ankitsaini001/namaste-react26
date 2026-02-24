914c8eb976a5d1db020e9c398c792bec0d0602d1
ankitsaini001 <ankitsaini594@gmail.com>
2026-02-25 00:07:33 +0530

    Learn about npm init, parcel and how we Igniting app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /***
  * <div id="parent">
  * <div id="child">
@@ -17,7 +19,7 @@
 const parent = React.createElement("div", {id: "parent"},
 [
         React.createElement("div", {id: "child"},[
-        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h1", {}, "I am Nmaste React"),
         React.createElement("h2", {}, "I am h2 tag")
     ]),
         React.createElement("div", {id: "child2"},[
