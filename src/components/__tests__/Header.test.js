6c6e244416ce13320a89eea99e120ba7cb5cfca9
ankitsaini001 <ankitsaini594@gmail.com>
2026-04-12 00:00:48 +0530

    header component unit header link testcases

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
@@ -18,5 +18,46 @@ it("should render the header component login button", () => {
     const loginButton = screen.getByText("Login");
 
     expect(loginButton).toBeInTheDocument();
-})    
+});
+
+it("should render the header component online status", () => {
+    render(
+        <Provider store={appStore}>
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>
+        </Provider>
+    );
+
+    const onlineStatus = screen.getByText(/Online Status:/i);
+
+    expect(onlineStatus).toBeInTheDocument();
+});
+
+it("should render the header component cart items", () => {
+    render(
+        <Provider store={appStore}>
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>
+        </Provider>
+    );
+    const cartItems = screen.getByText(/Cart - \(\d+ items\)/i);
+
+    expect(cartItems).toBeInTheDocument();
+});
+
+it("should render the header login link rendered onclick", () => {
+    render(
+        <Provider store={appStore}>
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>
+        </Provider>
+    );
+    const loginButton = screen.getByText("Login");
+    fireEvent.click(loginButton);
+    const loginButtonAfterClick = screen.getByText("Logout");
+    expect(loginButtonAfterClick).toBeInTheDocument();
+});
 });
