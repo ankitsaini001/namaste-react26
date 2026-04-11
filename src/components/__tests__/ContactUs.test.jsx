779231eaac46366bdc7f7306d35feed9658564a0
ankitsaini001 <ankitsaini594@gmail.com>
2026-04-11 22:52:31 +0530

    Grouping of testcases

--- src/components/__tests__/ContactUs.test.jsx
+++ src/components/__tests__/ContactUs.test.jsx
@@ -2,7 +2,8 @@ import { render, screen } from "@testing-library/react";
 import ContactUs from "../ContactUs";
 import "@testing-library/jest-dom";
 
-test("renders contact form correctly",() =>{
+describe("ContactUs component tests", () => {
+  test("renders contact form correctly", () => {
     // We first have to render the component to test it
     render(<ContactUs />);
 
@@ -11,28 +12,29 @@ test("renders contact form correctly",() =>{
 
     // we can expect the title to be in the document
     expect(titleElement).toBeInTheDocument();
-});
+  });
 
-test("render contact form email", () => {
+  test("render contact form email", () => {
     render(<ContactUs />);
-    const emailElement =  screen.getByText("Email");
+    const emailElement = screen.getByText("Email");
     expect(emailElement).toBeInTheDocument();
-});
+  });
 
-test("render contact form phone", () => {
+  test("render contact form phone", () => {
     render(<ContactUs />);
-    const phoneElement =  screen.getByText("Phone");
+    const phoneElement = screen.getByText("Phone");
     expect(phoneElement).toBeInTheDocument();
-});
+  });
 
-test("render contact form for contact-item", () => {
+  test("render contact form for contact-item", () => {
     render(<ContactUs />);
     const contactItems = screen.getAllByRole("paragraph");
     expect(contactItems.length).not.toBe(3);
-});
+  });
 
-test("render contact form for contact-item", () => {
+  it("render contact form for contact-item", () => {
     render(<ContactUs />);
     const contactItems = screen.getAllByRole("paragraph");
     expect(contactItems.length).toBe(4);
+  });
 });
